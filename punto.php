6d9f23c966e620ec425e4130a22b08e4ef253a6a
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-10-18 09:20:54 +0200

    Añadido clase Punto

---
+++ punto.php
@@ -0,0 +1,30 @@
+<?php
+
+    class Punto {
+        protected $x;
+        protected $y;
+    
+        public function __construct($x, $y) 
+        {
+            $this->x = $x;
+            $this->y= $y;
+        }
+
+        public function getX() {
+            return $this->x;
+        }
+
+        public function getY() {
+            return $this->y;
+        }
+
+        public function distancia($punto) {
+            // TODO comprobar
+            $primerParentesis = ($punto->x - $this->x) * ($punto->x - $this->x);
+
+            $segundoParentesis = ($punto->y - $this->y) * ($punto->y - $this->y);
+
+            return sqrt($primerParentesis + $segundoParentesis);
+        }
+    }
+?>
