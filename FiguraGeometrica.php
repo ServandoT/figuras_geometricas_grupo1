7760c29d943c2310c1286f5a8c5f0b302499cb86
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-10-18 09:35:23 +0200

    Añadido clase FiguraGeometrica

---
+++ FiguraGeometrica.php
@@ -0,0 +1,20 @@
+<?php
+    // TODO comprobar si está bien
+    include_once "Punto.php";
+
+    abstract class FiguraGeometrica {
+        protected $punto;
+        protected $nombre;
+
+        public function __construct($nombre)
+        {
+            $this->nombre = $nombre;
+        }
+
+        public abstract function getNombre();
+
+        public abstract function area();
+
+        public abstract function perimetro();
+    }
+?>
