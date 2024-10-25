a99b5eae875bcd86c4ca57ccc00e022329240355
JuanAntonioPanchon <martin.panchon.juan.antonio@iescamas.es>
2024-10-25 09:27:29 +0200

    Comprobado que la clase Triangulo funciona todas sus funciones

--- PruebasUnitarias/Prueba.php
+++ PruebasUnitarias/Prueba.php
@@ -1,7 +1,7 @@
 <?php
 // Incluye los archivos necesarios desde la carpeta src
-require_once __DIR__ . '/../src/FiguraGeometrica.php';
-require_once __DIR__ . '/../src/Triangulo.php';
+require_once __DIR__ . '/../FiguraGeometrica.php';
+require_once __DIR__ . '/../Triangulo.php';
 
 // Función para ejecutar una aserción manual de igualdad
 function assertEquals($expected, $actual, $testName) {
@@ -15,19 +15,33 @@ function assertEquals($expected, $actual, $testName) {
 // Pruebas unitarias simples para la clase Triangulo
 
 // Test 1: Prueba el cálculo del área de un Triangulo
-$triangulo = new Triangulo("Triangulo de prueba", 5, 5, 5, 5, 5, 5);
+$triangulo = new Triangulo("Triangulo1", 5, 5, 5, 5, 5, 5);
 $expectedArea = 5 * 5 / 2;
 $actualArea = $triangulo->area();
 assertEquals($expectedArea, $actualArea, "Prueba del cálculo del área");
 
 // Test 2: Prueba el cálculo del perímetro de un Triangulo
-$expectedPerimetro = $this->lado1 + $this->lado2 + $this->lado3;
+$expectedPerimetro = 15;
 $actualPerimetro = $triangulo->perimetro();
 assertEquals($expectedPerimetro, $actualPerimetro, "Prueba del cálculo del perímetro");
 
 // Test 3: Prueba si se retorna correctamente el nombre del triangulo
-$expectedNombre = "Triangulo de prueba";
+$expectedNombre = "Triangulo1";
 $actualNombre = $triangulo->getNombre();
 assertEquals($expectedNombre, $actualNombre, "Prueba de obtención del nombre");
 //
+
+// Test 4: Prueba si se retorna correctamente la base
+$expectedBase = 5;
+$actualBase = $triangulo->getBase();
+assertEquals($expectedNombre, $actualNombre, "Prueba de obtención de la base");
+//
+
+
+// Test 5: Prueba si se retorna correctamente la altura
+$expectedAltura = 5;
+$actualAltura = $triangulo->getAltura();
+assertEquals($expectedNombre, $actualNombre, "Prueba de obtención de la altura");
+//
+
 ?>
