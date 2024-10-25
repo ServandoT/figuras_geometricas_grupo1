daa91215590c5b3f000ba89f131e5861da25b62e
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-10-25 09:36:31 +0200

    Refactorizado

--- circulo_feature.php
+++ circulo_feature.php
@@ -1,28 +1,37 @@
 
 <?php
 include_once "FiguraGeometrica.php";
-class circulo extends FiguraGeometrica {
-//Variables
+class circulo extends FiguraGeometrica
+{
+    //Variables
     private $radio;
     private $nombre;
     private $punto;
-//Constructor
-    public function __construct($nombre, $punto, $radio) {
-        $this-> nombre = $nombre;
-        $this-> punto = $punto;
-        $this-> radio = $radio;
+    //Constructor
+    public function __construct($nombre, $punto, $radio)
+    {
+        $this->nombre = $nombre;
+        $this->punto = $punto;
+        $this->radio = $radio;
     }
-//Metodos
-   public function getRadio() {
-        return $this -> radio;
-   }
-   //Devuelve el area del circulo
-   private function area() {
-    return pi() * ($this-> radio * $this-> radio);
-   }
+    //Metodos
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+    public function getRadio()
+    {
+        return $this->radio;
+    }
+    //Devuelve el area del circulo
+    public function area()
+    {
+        return pi() * ($this->radio * $this->radio);
+    }
     //Devuelve el perímetro del circulo
-   private function perimetro($radio) {
-    return 2 * pi() * $this-> radio;
-   }
+    public function perimetro()
+    {
+        return 2 * pi() * $this->radio;
+    }
 }
 ?>
